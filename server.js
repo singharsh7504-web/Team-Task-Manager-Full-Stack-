53d7d0390b636a66abd07c48ac1ad2e587d425c8
singharsh7504-web <singharsh7504@gmail.com>
2026-04-30 16:25:38 +0000

    done

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.use('/api/notifications', require('./backend/routes/notifications'));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
-  app.get('*', (req, res) =>
+  app.get('/*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
   );
 } else {
